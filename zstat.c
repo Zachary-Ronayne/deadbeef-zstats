b247ca4cc2c3841d3b36f2ddde23acf20e827b18
zac <zronayne42@gmail.com>
2026-08-20 18:24:08 -0400

    Extract the song path and log it

--- zstat.c
+++ zstat.c
@@ -2,15 +2,68 @@
 /* Distributed under the terms of the 3-Clause BSD License. */
 /* Full license text available in 'LICENSE' file.           */
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include <deadbeef.h>
 
 // Main instance for deadbeef
 static DB_functions_t *deadbeef;
 
+static int songFinished(char *songPath){
+    deadbeef->log("song done %s", songPath);
+
+    return 0;
+}
+
+// Places the path in the given path pointer, returns 0 on success. Must free path if 0 is not returned
+static int copySongPath(ddb_event_track_t *event, char **path){
+    // Deadbeef lock
+    deadbeef->pl_lock();
+
+    // Get the path to the song
+    const char *songPath = deadbeef->pl_find_meta(event->track, ":URI");
+
+    // If it wasn't obtained, unlock and fail
+    if (!songPath) {
+        deadbeef->pl_unlock();
+        return -1;
+    }
+
+    // Copy the path out of the metadata
+    char* pathCopy = strdup(songPath);
+    *path = pathCopy;
+
+    // Deadbeef unlock after grabbing the data
+    deadbeef->pl_unlock();
+
+    return 0;
+}
+
 // Called when deadbeef triggers an event
 static int handle_event(uint32_t current_event, uintptr_t ctx, uint32_t p1, uint32_t p2){
-    deadbeef->log("zstats got event\n");
+    // Update stats when the track finishes playing
+    if(current_event == DB_EV_SONGFINISHED) {
+        // Get the data about the event
+        ddb_event_track_t *event = (ddb_event_track_t *)ctx;
+        // Get the path from the metadata
+        char *songPath;
+        int success = copySongPath(event, &songPath);
+
+        // Exit on fail
+        if(success != 0) return success;
+
+        // Handle treating the song as played
+        success = songFinished(songPath);
+
+        // Free the memory used for the path
+        free(songPath);
+
+        // Return if the process was successful
+        return success;
+    }
+
+    // Nothing to do, return success
     return 0;
 }
 
