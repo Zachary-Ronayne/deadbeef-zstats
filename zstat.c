1b56aceb0f8e897388b1922c4e117beb73a71c39
zac <zronayne42@gmail.com>
2026-08-21 23:14:33 -0400

    Dummy data loading into metadata fields to display on deadbeef

--- zstat.c
+++ zstat.c
@@ -3,9 +3,15 @@
 /* Full license text available in 'LICENSE' file.           */
 #include <stdio.h>
 #include <stdlib.h>
+#include <inttypes.h>
 #include <string.h>
 
 #include <deadbeef.h>
+
+// Metadata name for play count
+#define META_PLAY_COUNT "zstat_play_count"
+// Metadata name for last played timestamp
+#define META_LAST_PLAYED "zstat_last_played"
 
 // Main instance for deadbeef
 static DB_functions_t *deadbeef;
@@ -17,12 +23,12 @@ static int songFinished(char *songPath){
 }
 
 // Places the path in the given path pointer, returns 0 on success. Must free path if 0 is not returned
-static int copySongPath(ddb_event_track_t *event, char **path){
+static int copySongPath(DB_playItem_t *track, char **path){
     // Deadbeef lock
     deadbeef->pl_lock();
 
     // Get the path to the song
-    const char *songPath = deadbeef->pl_find_meta(event->track, ":URI");
+    const char *songPath = deadbeef->pl_find_meta(track, ":URI");
 
     // If it wasn't obtained, unlock and fail
     if (!songPath) {
@@ -40,15 +46,115 @@ static int copySongPath(ddb_event_track_t *event, char **path){
     return 0;
 }
 
+
+typedef struct Zstat{
+    int64_t play_count;
+    int64_t last_played;
+} zstat;
+
+// Find the stats for the given value
+static void findsStat(DB_playItem_t *track, zstat *stat){
+    // Grab the path from the track
+    char *songPath;
+    int success = copySongPath(track, &songPath);
+    deadbeef->log("found path %s\n", songPath);
+
+    // TODO use path to get stats
+
+    // Free the memory from the copy
+    free(songPath);
+
+    // Update stats on the stat struct
+    stat->play_count = 67;
+    // TODO figure out how to format this in the ui
+    stat->last_played = 1787279087000;
+}
+
+// Places a string representation of the given number in str
+static void stringValue(int64_t number, char *str, size_t stringSize){
+    snprintf(str, stringSize, "%" PRId64, number);
+}
+
+// Places the number into the deadbeef meta for the given name
+static void updateIntMeta(int64_t number, DB_playItem_t *track, char *name){
+    // Get the string value from the number
+    char numberStr[32];
+    stringValue(number, numberStr, sizeof(numberStr));
+    // Store that string in the metadata
+    deadbeef->pl_replace_meta(track, name, numberStr);
+
+}
+
+// Update the stats of the given track
+static int updateStat(DB_playItem_t *track){
+    zstat stat;
+    findsStat(track, &stat);
+
+    updateIntMeta(stat.play_count, track, META_PLAY_COUNT);
+    updateIntMeta(stat.last_played, track, META_LAST_PLAYED);
+    return 0;
+}
+
+// Update the stats of all records when the plugin loads
+static int updateStats(void){
+    DB_playItem_t *track = deadbeef->pl_get_first(PL_MAIN);
+
+    while(track){
+        updateStat(track);
+
+        // TODO is this setting anything?
+        // deadbeef->pl_lock();
+        // deadbeef->pl_set_meta_int(track, "test", 69);
+        // deadbeef->pl_unlock();
+
+        DB_playItem_t *next = deadbeef->pl_get_next(track, PL_MAIN);
+        deadbeef->pl_item_unref(track);
+        track = next;
+    }
+
+    return 0;
+}
+
+// Run when deadbeef connects the plugin
+static int connect(void){
+    return updateStats();
+}
+
+// Run when deadbeef starts
+static int start(void){
+    return 0;
+}
+
+// Called when the plugin stops
+static int stop(void) {
+    deadbeef->log("zstats stop\n");
+    return 0;
+}
+
 // Called when deadbeef triggers an event
 static int handle_event(uint32_t current_event, uintptr_t ctx, uint32_t p1, uint32_t p2){
+
+    DB_playItem_t *track = deadbeef->pl_get_first(PL_MAIN);
+
+    if(current_event == DB_EV_PLAYLISTCHANGED){
+        if (track) {
+            // TODO see if this updates too frequently
+            deadbeef->log("event update stat\n");
+            updateStats();
+        }
+    }
+
+
+
+
+
     // Update stats when the track finishes playing
     if(current_event == DB_EV_SONGFINISHED) {
         // Get the data about the event
         ddb_event_track_t *event = (ddb_event_track_t *)ctx;
         // Get the path from the metadata
         char *songPath;
-        int success = copySongPath(event, &songPath);
+        int success = copySongPath(event->track, &songPath);
 
         // Exit on fail
         if(success != 0) return success;
@@ -67,18 +173,6 @@ static int handle_event(uint32_t current_event, uintptr_t ctx, uint32_t p1, uint
     return 0;
 }
 
-// Run when deadbeef starts
-static int start(void){
-    deadbeef->log("zstats start\n");
-    return 0;
-}
-
-// Called when the plugin stops
-static int stop(void) {
-    deadbeef->log("zstats stop\n");
-    return 0;
-}
-
 // Define the plugin
 static DB_misc_t plugin = {
     .plugin = {
@@ -87,8 +181,8 @@ static DB_misc_t plugin = {
         .api_vmajor = 1,
         .api_vminor = 10,
 
-        .version_major = 3,
-        .version_minor = 0,
+        .version_major = 0,
+        .version_minor = 1,
 
         .id = "zstat",
 
@@ -107,7 +201,7 @@ static DB_misc_t plugin = {
 
         .start = start,
         .stop = stop,
-        .connect = NULL,
+        .connect = connect,
         .disconnect = NULL,
 
         .exec_cmdline = NULL,
